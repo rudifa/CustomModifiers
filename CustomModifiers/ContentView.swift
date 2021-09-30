44d2a30ab8f0d891aba0f96fee99140d60cee2b8
Rudi Farkas <rudi.farkas@gmail.com>
2021-09-30 15:21:04 +0200

    ContentView: add NavigationView List with ShapeStrokeAndFill

--- CustomModifiers/ContentView.swift
+++ CustomModifiers/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            List {
+                NavigationLink(destination: { ShapeStrokeAndFill() }, label: { Text("ShapeStrokeAndFill") })
+            }
+            .navigationBarTitle(Text("Demos"))
             .padding()
+        }
     }
 }
 

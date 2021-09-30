b0c51b935df55b39b09c0170e1a0105f61e19efb
Rudi Farkas <rudi.farkas@gmail.com>
2021-09-30 13:43:10 +0200

    Initial Commit

---
+++ CustomModifiers/CustomModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomModifiersApp.swift
+//  CustomModifiers
+//
+//  Created by Rudolf Farkas on 30.09.21.
+//
+
+import SwiftUI
+
+@main
+struct CustomModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CustomModifiersUITests/CustomModifiersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CustomModifiersUITestsLaunchTests.swift
+//  CustomModifiersUITests
+//
+//  Created by Rudolf Farkas on 30.09.21.
+//
+
+import XCTest
+
+class CustomModifiersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
